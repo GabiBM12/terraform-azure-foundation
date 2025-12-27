efd8115085560df0c8e71993899c637b441c59f1
Gabi Bostan <gabibostan@Mac.broadband>
2025-12-27 18:21:03 +0000

    feat(dev): expose foundation outputs

---
+++ envs/dev/outputs.tf
@@ -0,0 +1,12 @@
+output "resource_group_name" {
+  description = "The name of the resource group"
+  value       = azurerm_resource_group.this.name
+}
+output "subnet_ids" {
+  description = "The IDs of the subnets"
+  value       = module.network.subnet_ids
+}
+output "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics workspace"
+  value       = module.monitoring.workspace_id
+}
